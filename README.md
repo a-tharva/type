2cef0194624bb66c6cb447e9adf9904fcccd01d3
atharva bhandvalkar <atharv.bhandvalkar@gmail.com>
2021-11-17 11:43:38 +0530

    added changelog and gitignore

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# type
+# typePrint
+
+## Description
+
+Still in development.
+
+This module will print like someone is typing.
+
+## Installation
+
+```bash
+> pip install typePrint
+```
+
+## Use within another Python script
+
+```python
+>>> import typePrint as tp
+
+>>> tp.typeHuman('Some text here')
+Some text here
+```
--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.0.1'
+VERSION = '0.0.1.4'
 DESCRIPTION = 'Print like someone is typing'
 LONG_DESCRIPTION = 'A package that print provided string letter by letter'
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
@@ -23,9 +27,10 @@ setup(
     author="Atharva Bhandvalkar",
     author_email="<atharv.bhandvalkar@gmail.com>",
     license='MIT',
+    url='https://github.com/a-tharva/type',
     description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[],
     keywords=['python'],
